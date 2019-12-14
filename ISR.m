b49f7d70d61564c6ab7b3aeb138f4f73a5d18927
Meng Luo <czheluo@gmail.com>
2019-12-14 19:58:31 +0800

    Update ISR.m

--- ISR.m
+++ ISR.m
@@ -3,8 +3,6 @@ function []=ISR(varargin)
     % ISR methods for GWAS @AUTHOR MENG LUO  %
     % contact: czheluo@gmail.com             %  
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %seting the env path 
-    % alias matlab='/mnt/d/linux/MATLAB2016b/bin/matlab -nodesktop -nosplash -singleCompThread -logfile `date +%Y_%m_%d-%H_%M_%S`.log -r'
     addpath([pwd,'/src/']);
     phefile = ft_getopt(varargin, 'phefile', 'phe.fam');
     genofile = ft_getopt(varargin, 'genofile', 'pop.traw');
@@ -20,6 +18,22 @@ function []=ISR(varargin)
     ncov = ft_getopt(varargin, 'ncov', []);
     IM = ft_getopt(varargin, 'IM', 1);
     sgv = ft_getopt(varargin, 'sgv', 0.05); % default bonferroni correction
+    mdl = ft_getopt(varargin, 'model',1); 
+    %phefile = string, can be any of file format split with "\t"(default = 'phe.fam')
+    %genofile = string, .traw file format from plink (default = 'pop.traw')
+    %outfile = string, save covert genotypes file name with any name you defined and save matlab format (default = 'pop.traw.mat')
+    %sample = number, the number of individuals you want to analysis
+    %nSNP = number, the number of SNPs.
+    %ntrait = number, the number of traits.
+    %chr = number, the number of chromosome.
+    %opt_outresult = string, write the result to text file (default = 'ISR.opt.outresult.txt') 
+    %all_outresult = string, write the result to text file (default = 'ISR.outresult.txt')  
+    %vcf = string, the VCF file name.
+    %bed = string, the bed file name.
+    %ncov = number, the number of PCs covariates.
+    %IM = impute missing genotype with mean and median value, '1' was the default method means and others was median.
+    %sgv = number, the bonferroni correction for association tests results.
+    %mdl = number,1 for linear model and 2 or 3 for nolinear model ; input('Using Model II(without square term 2) or Model III(with square term 3) 2/3? ');
     % Usage:
     %       matlab "ISR_linux('phefile','../data/pop.fam','genofile','../data/pop.traw','sample',87,'nSNP',28228,'ntrait',1,'ncov',5),exit;"
            
@@ -35,21 +49,14 @@ function []=ISR(varargin)
     %end
     %maxNumCompThreads=1;
     % write a shell script for ISR getopt....
-    if ~isempty(vcf)
-        system(['plink ',' --vcf ',vcf,' --recode A-transpose --out pop']);
-        traw2mat(phefile,genofile,outfile,sample,nSNP,ntrait,IM);
-    elseif ~isempty(bed)
-        system(['plink ',' --bfile ',bed,' --recode A-transpose --out pop ']);
-        traw2mat(phefile,genofile,outfile,sample,nSNP,ntrait,IM);
-    else
-        traw2mat(phefile,genofile,outfile,sample,nSNP,ntrait,IM);
-    end
+    traw2mat(phefile,genofile,outfile,sample,nSNP,ntrait,IM);
     %traw2mat('../../pop.fam','../../poptraw.traw','popnew.mat',175,407045,3)
     load(outfile);
     Y=y;
     [~,p1]=size(Y);
     if p1==1
         %clear,clc
+        yname=2;
         warning('off','all')
         diary('ISR.log'); % notes the command window diary
         impute=2;yname=2;
@@ -71,12 +78,20 @@ function []=ISR(varargin)
         % end
         %stop
         % ISR method
-        if ~isempty(ncov)
-            ISR_COV
+        if mdl==2
+            if ~isempty(ncov)
+                ISR_Epi_COV
+            else
+                ISR_Epi
+            end
         else
-            ISR_REG
-        end
-        %changing possible the last association results
+            if ~isempty(ncov)
+                ISR_COV
+            else
+                ISR_REG
+            end
+        end     
+       %changing possible the last association results
         writetable(opt_result,opt_outresult,'Delimiter','\t');writetable(all_result,all_outresult,'Delimiter','\t');
         diary off
         man=figure(1);
@@ -119,10 +134,18 @@ function []=ISR(varargin)
             % end
             %stop
             % ISR method
-            if ~isempty(ncov)
-                ISR_COV
+            if mdl==2
+                if ~isempty(ncov)
+                    ISR_Epi_COV
+                else
+                    ISR_Epi
+                end
             else
-                ISR_REG
+                if ~isempty(ncov)
+                    ISR_COV
+                else
+                    ISR_REG
+                end
             end
             %changing possible the last association results
             writetable(opt_result,opt_outresult,'Delimiter','\t');writetable(all_result,all_outresult,'Delimiter','\t');
@@ -147,4 +170,5 @@ function []=ISR(varargin)
             %clf;
         end
     end
-end    
+end
+   
